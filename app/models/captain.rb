412a6235ef21755b045f274de0eb604815c096b0
MartinCrane <martin.crane@gmail.com>
2017-02-17 15:32:16 -0500

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,50 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  has_many :boat_classifications, through: :boats
+  has_many :classifications, through: :boat_classifications
+
+  def self.catamaran_operators
+
+    # all.joins(:boat_classifications, :classifications).where("classifications.name = 'Catamaran'").distinct
+    all.joins(boats: :classifications).where(
+      classifications: {name: "Catamaran"}
+
+    )
+  end
+
+  def self.sailors
+      # all.joins(:boat_classifications, :classifications).where("classifications.name = 'Sailboat'").distinct
+      get
+      all.joins(boats: :classifications).where(classifications)
+  end
+
+  def self.with_classificaitons
+      joins(boats: :classifications)
+  end
+
+  def self.sailors
+    get_boat_type("Sailboat")
+  end
+
+  def self.get_boat_type(x)
+     with_classificaitons.where(classifications: { name:x }).uniq
+  end
+
+  def self.talented_seamen
+
+    # motor = all.joins(:classifications).where(classifications: {name: ['Motorboat']})
+    # sail = all.joins(:classifications).where(classifications: {name: ['Sailboat']})
+    #
+    #
+    # find_by_sql("#{motor.to_sql} INTERSECT #{sail.to_sql}")
+
+
+    # and classifications.name = ?", 'Sailboat',
+    all.joins(:boat_classifications, :classifications).where(classifications: {name: ['Motorboat', 'Sailboat']}).group("classifications.name").order("captains.name")
+  end
+
+  def self.non_sailors
+
+    all.joins(:boat_classifications, :classifications).select("captains.name").where.not(captains: {name: sailors.pluck}).distinct
+  end
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,18 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+
+    all.joins(:boat_classifications).distinct
+  end
+
+  def self.longest
+    
+    # type = select("boats.name").joins(boats: :boat_classifications).group("boats.name").order("boats.length desc").limit(1)
+    Boat.joins(:classifications).order("boats.length desc").first.classifications
+
+    # self.joins(:boats).order("boats.length desc")
+
+  end
 end
